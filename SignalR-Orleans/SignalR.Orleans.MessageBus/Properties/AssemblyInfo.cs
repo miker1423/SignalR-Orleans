33879cf3cd9951c37e0bfdbb0843f85740fb6c8f
Miguel Pérez García <miguelpg_95@hotmail.com>
2016-10-10 10:51:43 -0500

    Starting

---
+++ SignalR-Orleans/SignalR.Orleans.MessageBus/Properties/AssemblyInfo.cs
@@ -0,0 +1,19 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("SignalR.Orleans.MessageBus")]
+[assembly: AssemblyTrademark("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("884ccc09-b9e3-43b4-8d3c-9d20f4090e0d")]
